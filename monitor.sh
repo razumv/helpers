3e4636de24055abe4add82ce888e670050b0f351
razumv <42905535+razumv@users.noreply.github.com>
2021-08-04 10:04:10 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -41,7 +41,7 @@ scrape_configs:
   - job_name: "prometheus"
     scrape_interval: 30s
     static_configs:
-      - targets: ["localhost:39190"]
+      - targets: ["localhost:49190"]
     relabel_configs:
       - source_labels: [__address__]
         regex: '.*'
@@ -77,7 +77,7 @@ sudo tee <<EOF >/dev/null /etc/systemd/system/prometheus.service
   --web.console.templates=/etc/prometheus/consoles \
   --web.console.libraries=/etc/prometheus/console_libraries \
   --storage.tsdb.retention.time=30m \
-  --web.listen-address="0.0.0.0:39190"
+  --web.listen-address="0.0.0.0:49190"
   ExecReload=/bin/kill -HUP $MAINPID
 [Install]
   WantedBy=multi-user.target
