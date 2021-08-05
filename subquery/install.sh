6e27395b4f4a9473cbbdbc5d9834f0425e189758
razumv <42905535+razumv@users.noreply.github.com>
2021-08-05 13:35:47 +0300

    Update install.sh

--- subquery/install.sh
+++ subquery/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! $SUBQUERY_NODENAME ]; then
+	read -p "Введите имя ноды: " SUBQUERY_NODENAME
+fi
+echo 'Имя ноды: ' $SUBQUERY_NODENAME
+sleep 1
+echo 'export SUBQUERY_NODENAME='$SUBQUERY_NODENAME >> $HOME/.profile
+
 sudo apt update
 sudo curl https://deb.nodesource.com/setup_14.x | sudo bash
 sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -11,11 +18,15 @@ sudo sh get-docker.sh
 sudo apt install -y docker-compose
 rm -f get-docker.sh
 
-git clone https://github.com/subquery/subql-helloworld.git
-cd subql-helloworld
+git clone https://github.com/subquery/subql-init
+npm install -g @subql/cli
+subql init --init $SUBQUERY_NODENAME
 yarn
+npm install
 yarn codegen
+npm run-script codegen
 yarn build
+npm run-script build
 
 docker-compose up -d
 
