e5d691804c4d6834ea35918640eefa11b15e4881
razumv <42905535+razumv@users.noreply.github.com>
2021-08-07 00:01:00 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -14,23 +14,8 @@ echo 'export HOSTNAME='$HOSTNAME >> $HOME/.profile
 
 systemctl stop prometheus && systemctl disable prometheus
 
-# sudo useradd --no-create-home --shell /usr/sbin/nologin prometheus
 sudo mkdir /etc/prometheus
-# sudo mkdir /var/lib/prometheus
-# sudo chown -R prometheus:prometheus /etc/prometheus
-# sudo chown -R prometheus:prometheus /var/lib/prometheus
-# wget https://github.com/prometheus/prometheus/releases/download/v2.25.2/prometheus-2.25.2.linux-amd64.tar.gz
-# tar xfz prometheus-*.tar.gz
-# cd prometheus-2.25.2.linux-amd64
-# sudo cp ./prometheus /usr/local/bin/
-# sudo cp ./promtool /usr/local/bin/
-# sudo chown prometheus:prometheus /usr/local/bin/prometheus
-# sudo chown prometheus:prometheus /usr/local/bin/promtool
-# sudo cp -r ./consoles /etc/prometheus
-# sudo cp -r ./console_libraries /etc/prometheus
-# sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-# sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
-# cd .. && rm -rf prometheus*
+
 apt install wget -y 
 wget https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.63.0/vmutils-amd64-v1.63.0.tar.gz
 tar xvf vmutils-amd64-v1.63.0.tar.gz
@@ -55,8 +40,6 @@ scrape_configs:
         replacement: '$HOSTNAME'         
 EOF
 
-# sudo chown prometheus:prometheus /etc/prometheus/prometheus.yml
-
 sudo tee <<EOF >/dev/null /etc/systemd/system/vmagent.service
 [Unit]
   Description=vmagent Monitoring
@@ -78,7 +61,6 @@ sudo systemctl daemon-reload && systemctl enable vmagent && systemctl restart vm
 wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
 tar xvf node_exporter-1.1.2.linux-amd64.tar.gz
 cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin
-sudo chown prometheus:prometheus /usr/local/bin/node_exporter
 
 sudo tee <<EOF >/dev/null /etc/systemd/system/node_exporter.service
 [Unit]
