9a60a21539e6a0a8a7047ba39ee7471be4a441fd
razumv <42905535+razumv@users.noreply.github.com>
2021-08-06 13:11:38 +0300

    Update update_miner.sh

--- aleo/update_miner.sh
+++ aleo/update_miner.sh
@@ -4,13 +4,7 @@ sudo systemctl stop miner
 cd $HOME/snarkOS
 git fetch
 git checkout v1.3.13
-#cargo build --release --verbose
-cd
-wget https://github.com/AleoHQ/snarkOS/releases/download/v1.3.13/aleo-testnet1-v1.3.13-x86_64-unknown-linux-gnu.zip
-unzip aleo-testnet1-v1.3.13-x86_64-unknown-linux-gnu.zip
-mkdir -p $HOME/snarkOS/target/release/
-mv snarkos $HOME/snarkOS/target/release/
-rm -f aleo-testnet1-v1.3.13-x86_64-unknown-linux-gnu.zip
+cargo build --release --verbose
 rm -rf $HOME/.snarkOS/snarkos_testnet1
 rm -rf $HOME/.snarkOS/snarkos_testnet1_secondary
 cd
