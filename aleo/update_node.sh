41d26d68544bea3d43bf8a090f690db79dfe2a49
razumv <42905535+razumv@users.noreply.github.com>
2021-08-06 11:46:31 +0300

    Create update_node.sh

---
+++ aleo/update_node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+systemctl stop aleo
+cd $HOME/snarkOS
+git fetch
+git checkout v1.3.13
+cargo build --release --verbose
+rm -rf $HOME/.snarkOS/snarkos_testnet1
+rm -rf $HOME/.snarkOS/snarkos_testnet1_secondary
+cd
+wget 188.166.34.137/backup_snarkOS_347400.tar.gz
+tar xvf backup_snarkOS_347400.tar.gz
+mv backup_snarkOS_347400/.snarkOS/* $HOME/.snarkOS/
+rm -rf backup_snarkOS_347400*
+systemctl start aleo
+
+version=`$HOME/snarkOS/target/release/snarkos help | grep snarkOS | head -n 1`
+echo 'Current version' $version
