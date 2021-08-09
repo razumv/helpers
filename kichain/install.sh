49ea2a11a16ca708c2bed73369ac4e864430466a
razumv <42905535+razumv@users.noreply.github.com>
2021-08-09 19:46:34 +0300

    Update install.sh

--- kichain/install.sh
+++ kichain/install.sh
@@ -32,15 +32,15 @@ mkdir -p $NODE_ROOT/kid $NODE_ROOT/kicli $NODE_ROOT/kilogs
 cd $NODE_ROOT
 
 kid init $KICHAIN_NODENAME --chain-id kichain-t-2 --home ./kid/
-kicli keys add $KICHAIN_NODENAME --home ./kicli/ &>> $NODE_ROOT/account.txt
-kid add-genesis-account $(kicli keys show $KICHAIN_NODENAME -a --home kicli/ ) 100000000utki --home kid/
-kid gentx \
-	--commission-max-change-rate=0.1 \
-	--commission-max-rate=0.1 \
-	--commission-rate=0.1 \
-	--min-self-delegation=1 \
-	--amount=100000000utki \
-	--pubkey `kid tendermint show-validator --home ./kid/` \
-	--name=$KICHAIN_NODENAME \
-	--home ./kid/ \
-	--home-client ./kicli/
+#kicli keys add $KICHAIN_NODENAME --home ./kicli/ &>> $NODE_ROOT/account.txt
+#kid add-genesis-account $(kicli keys show $KICHAIN_NODENAME -a --home kicli/ ) 100000000utki --home kid/
+#kid gentx \
+#	--commission-max-change-rate=0.1 \
+#	--commission-max-rate=0.1 \
+#	--commission-rate=0.1 \
+#	--min-self-delegation=1 \
+#	--amount=100000000utki \
+#	--pubkey `kid tendermint show-validator --home ./kid/` \
+#	--name=$KICHAIN_NODENAME \
+#	--home ./kid/ \
+#	--home-client ./kicli/
