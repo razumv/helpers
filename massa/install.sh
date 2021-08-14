0f981c66e86e16930b740714d46ca28fdd180e14
razumv <42905535+razumv@users.noreply.github.com>
2021-08-14 22:07:01 +0300

    Update install.sh

--- massa/install.sh
+++ massa/install.sh
@@ -31,6 +31,12 @@ ExecStart=$HOME/massa/target/release/massa-node
 [Install]
 WantedBy=multi-user.target
 EOF
+
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+sudo systemctl restart systemd-journald
 sudo systemctl enable massa
 sudo systemctl daemon-reload
 sudo systemctl restart massa
