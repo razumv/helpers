24ca8ce48eca33f5c8501a333d5b194f088145ff
razumv <razumv95@gmail.com>
2021-08-17 23:36:14 +0300

    fix rust aleo

--- aleo/install_node.sh
+++ aleo/install_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install curl make clang pkg-config libssl-dev build-essential git mc jq unzip wget -y
-curl https://getsubstrate.io -sSf | bash -s -- --fast
+sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 sleep 1
 
