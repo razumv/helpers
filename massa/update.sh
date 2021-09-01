e70257ccb0a81110266c2ba78ee0f39223af9983
razumv <razumv95@gmail.com>
2021-09-01 21:45:47 +0300

    fix

--- massa/update.sh
+++ massa/update.sh
@@ -22,8 +22,9 @@ cp $HOME/bk/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 cd $HOME/massa/massa-client/
 cargo build --release
 cp $HOME/bk/wallet.dat $HOME/massa/massa-client/wallet.dat
+
+sudo systemctl start massa
+
 massa_wallet_address=$(cargo run --release wallet_info | grep Address | awk '{print $2}')
 cargo run --release -- buy_rolls $massa_wallet_address 20 0
 cargo run --release -- register_staking_keys $(cargo run --release wallet_info | jq -r ".wallet[0]")
-
-sudo systemctl start massa
