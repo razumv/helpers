8415f3d9cdca5e5adf1b3b90e571ee9d3cf7398d
razumv <razumv95@gmail.com>
2021-09-05 20:25:32 +0300

    fix

--- polkadex/install.sh
+++ polkadex/install.sh
@@ -37,7 +37,9 @@ Wants=network-online.target
 [Service]
 User=$USER
 ExecStart=$HOME/Polkadex/target/release/polkadex-node --rpc-cors=all --chain=$HOME/customSpecRaw.json --bootnodes /ip4/13.235.92.50/tcp/30333/p2p/12D3KooWL8Kcdvrhp5JyTYFeXSS5HR9csJnaLmJWRJYxnKakjBby --pruning=archive --validator --name '$POLKADEX_NODENAME | DOUBLETOP'
-Restart=on-failure
+Restart=always
+RestartSec=3
+LimitNOFILE=10000
 
 [Install]
 WantedBy=multi-user.target
