9b0f74b50ec6e7184a8d69c0cc2c282bea843a2e
razumv <razumv95@gmail.com>
2021-09-07 23:14:45 +0300

    fix

--- massa/update.sh
+++ massa/update.sh
@@ -25,7 +25,7 @@ cp $HOME/bk/wallet.dat $HOME/massa/massa-client/wallet.dat
 
 sudo systemctl start massa
 sleep 10
-
-massa_wallet_address=$(cargo run --release wallet_info | grep Address  |awk '{print $2}')
-cargo run --release -- buy_rolls $massa_wallet_address 1 0
-cargo run --release -- register_staking_keys $(cargo run --release wallet_info | grep Private | awk '{print $3}')
+echo DONE
+#massa_wallet_address=$(cargo run --release wallet_info | grep Address  |awk '{print $2}')
+#cargo run --release -- buy_rolls $massa_wallet_address 1 0
+#cargo run --release -- register_staking_keys $(cargo run --release wallet_info | grep Private | awk '{print $3}')
