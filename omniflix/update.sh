58976d88bd602b0000d85d596309a78aed17434a
razumv <razumv95@gmail.com>
2021-09-16 18:02:42 +0300

    fix update script

--- omniflix/update.sh
+++ omniflix/update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-cd $HOME/omniflixhub
-git fetch --all
-git checkout v0.2.2
-make install
-sudo systemctl restart omniflixhubd
+version=`omniflixhubd version`
+if [[ $version != "0.2.2" ]]; then
+  cd $HOME/omniflixhub
+  git fetch --all
+  git checkout v0.2.2
+  make install
+  sudo systemctl restart omniflixhubd
+fi
