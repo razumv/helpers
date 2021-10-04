3c87e3a50ccbd623107eee4a46f0a8cc92cedb10
razumv <razumv95@gmail.com>
2021-10-04 09:50:14 +0300

    fix

--- tools/install_ufw.sh
+++ tools/install_ufw.sh
@@ -5,7 +5,7 @@ sudo apt install ufw -y
 sudo ufw allow 22:65535/tcp
 sudo ufw allow 22:65535/udp
 sudo ufw deny out from any to 10.0.0.0/8
-sudo ufw deny out from any to 172.16.0.0/12
+#sudo ufw deny out from any to 172.16.0.0/12
 sudo ufw deny out from any to 192.168.0.0/16
 sudo ufw deny out from any to 100.64.0.0/10
 sudo ufw deny out from any to 198.18.0.0/15
