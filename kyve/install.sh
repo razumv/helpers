4b31c6fd24309e560cf2413c209ceda588632ad5
razumv <razumv95@gmail.com>
2021-10-08 13:17:50 +0300

    fix

--- kyve/install.sh
+++ kyve/install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 if [ ! $KYVE_NODENAME ]; then
-	read -p "Enter node name: " KYVE_NODENAME
+	read -p "Введите имя ноды: " KYVE_NODENAME
 fi
-echo 'Your node name: ' $KYVE_NODENAME
+echo 'Ваше имя ноды: ' $KYVE_NODENAME
 sleep 1
 echo 'export KYVE_NODENAME='$KYVE_NODENAME >> $HOME/.profile
 source .profile
 sleep 1
 
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_nvm_yarn_npm.sh | bash
 
 git clone https://github.com/KYVENetwork/kyve.git
 
 tee <<EOF >/dev/null $HOME/kyve/integrations/config.json
 {
   "pools": {
-    "0": 1,
-    "2": 10
+    "0xbBBfbE9A731634eDdf84C67A106CEE1F981F3f7e": 10
   }
 }
 EOF
@@ -26,10 +26,19 @@ tee <<EOF >/dev/null $HOME/kyve/integrations/node/.env
 CONFIG=config.json
 WALLET=arweave.json
 SEND_STATISTICS=true
+PK=private_key_from_metamask
 EOF
 
-cp $HOME/arweave.json $HOME/kyve/integrations/node/
+if [ ! -e $HOME/arweave.json ]; then
+	echo "Файл от расширения arweave.json отсутствует"
+else
+  cp $HOME/arweave.json $HOME/kyve/integrations/node/
+fi
+
+cd $HOME/kyve
+yarn setup
 
 cd $HOME/kyve/integrations/node
-docker build -t kyve-node:latest .
-docker run --name $KYVE_NODENAME kyve-node:latest
+yarn node:build
+
+docker run -d -it --restart=always --name=kyve kyve-node:latest
