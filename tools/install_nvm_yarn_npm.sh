9352c326a0b76051af36bdb9b12351b1202a2587
razumv <razumv95@gmail.com>
2021-10-08 13:09:04 +0300

    add yarn install

---
+++ tools/install_nvm_yarn_npm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install curl gnupg -y
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.profile
+nvm install 14.18.0
+nvm use 14.18.0
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install --no-install-recommends yarn
+yarn --version
+npm --version
+node --version
