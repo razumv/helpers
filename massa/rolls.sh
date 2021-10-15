34a371e92216b881864e52176c0e4165af017b01
razumv <razumv95@gmail.com>
2021-10-15 18:00:16 +0300

    fix

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #Thank's for https://raw.githubusercontent.com/bobu4/massa/main/bal.sh
+cd $HOME
+wget https://raw.githubusercontent.com/razumv/helpers/main/massa/massa-client
+chmod +x massa-client
 
-cd $HOME/massa/massa-client
-massa_wallet_address=$(cargo run --release wallet_info | jq -r '.balances | keys[0]')
+massa_wallet_address=$($HOME/massa-client --cli true wallet_info | jq -r '.balances | keys[0]')
 while true
 do
-	balance=$(cargo run --release wallet_info | jq -r '.balances[].final_ledger_data.balance')
+	balance=$($HOME/massa-client --cli true wallet_info | jq -r '.balances[].final_ledger_data.balance')
 	int_balance=${balance%%.*}
 	if [ $int_balance -gt "99" ]; then
 		echo "More than 99"
-		resp=$(cargo run --release buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+		resp=$($HOME/massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
 		echo $resp
 	elif [ $int_balance -lt "100" ]; then
 		echo "Less than 100"
