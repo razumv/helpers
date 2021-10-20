545358b07e4c829b73b68a503a46d2ad82004c5a
razumv <razumv95@gmail.com>
2021-10-20 10:30:47 +0300

    add peers and fix install

--- evmos/install.sh
+++ evmos/install.sh
@@ -32,14 +32,13 @@ evmosd config chain-id evmos_9000-1 &>/dev/null
 evmosd config keyring-backend file &>/dev/null
 evmosd init "$EVMOS_NODENAME" --chain-id evmos_9000-1 &>/dev/null
 wget -qO $HOME/.evmosd/config/genesis.json https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/genesis.json &>/dev/null
-live_peers="72160278cbce6192d376816fd715705eb41bc56a@194.163.187.219:26656,c893e2bf76b60099eecc20d4a9671ed9d4114464@65.21.193.112:26656"
 bootstrap_node="http://5.189.156.65:26657"; \
 latest_height=`wget -qO- "${bootstrap_node}/block" | jq -r ".result.block.header.height"`; \
 block_height=$((latest_height - 2000)); \
 trust_hash=`wget -qO- "${bootstrap_node}/block?height=${block_height}" | jq -r ".result.block_id.hash"`; \
 sed -i -e "s%^moniker *=.*%moniker = \"$EVMOS_NODENAME\"%; "\
 "s%^seeds *=.*%seeds = \"c36cec90ded95d162b85f8ecd00ecd7c8849ca75@arsiamons.seed.evmos.org:26656,`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/seeds.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
-"s%^persistent_peers *=.*%persistent_peers = \"847e72f31e1f87e8059231b4b9e3302989c22d3a@5.189.156.65:26656,$live_peers,`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/peers.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
+"s%^persistent_peers *=.*%persistent_peers = \"847e72f31e1f87e8059231b4b9e3302989c22d3a@5.189.156.65:26656,`wget -qO - https://raw.githubusercontent.com/razumv/helpers/main/evmos/peers.txt | tr '\n' ',' | sed 's%,$%%',`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/main/arsia_mons/peers.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
 "s%^enable *=.*%enable = false%; "\
 "s%^rpc_servers *=.*%rpc_servers = \"${bootstrap_node},${bootstrap_node}\"%; "\
 "s%^trust_height *=.*%trust_height = $block_height%; "\
