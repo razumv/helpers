b29fb1bac276375f73c2bd8b674add8e4083efce
razumv <razumv95@gmail.com>
2021-10-21 13:26:12 +0300

    add update kyve

---
+++ kyve/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker pull kyve/evm:latest &>/dev/null
+docker stop kyve &>/dev/null
+docker container rm kyve &>/dev/null
+docker run -d -it --restart=always \
+--name kyve kyve/evm:latest \
+--pool 0x753924e3f7bdbC877D1D81dD82A61c29a165814E \
+--private-key `cat $HOME/metamask.txt` \
+--stake 101 &>/dev/null
+echo DONE
