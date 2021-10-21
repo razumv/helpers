69148fc0c502baa31eb4ad56107875e2ad33b951
razumv <razumv95@gmail.com>
2021-10-21 13:34:22 +0300

    fix install

--- kyve/install.sh
+++ kyve/install.sh
@@ -7,7 +7,7 @@ echo "Устанавливаем софт"
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash &>/dev/null
-curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_node14.sh | bash &>/dev/null
+# curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_node14.sh | bash &>/dev/null
 sudo apt install --fix-broken -y &>/dev/null
 sudo apt install nano mc -y &>/dev/null
 source .profile
@@ -16,48 +16,56 @@ sleep 1
 echo "Весь необходимый софт установлен"
 echo "-----------------------------------------------------------------------------"
 
-if [ ! -d $HOME/kyve/ ]; then
-  git clone https://github.com/KYVENetwork/kyve.git &>/dev/null
-fi
+# if [ ! -d $HOME/kyve/ ]; then
+#   git clone https://github.com/KYVENetwork/kyve.git &>/dev/null
+# fi
 
 if [ ! -e $HOME/metamask.txt ]; then
 	echo "Файл с приватником от ММ отсутствует"
   exit 1
 fi
 
-if [ ! -e $HOME/arweave.json ]; then
-	echo "Файл от расширения arweave.json отсутствует"
-  exit 1
-else
-  cp $HOME/arweave.json $HOME/kyve/integrations/node/
-fi
+# if [ ! -e $HOME/arweave.json ]; then
+# 	echo "Файл от расширения arweave.json отсутствует"
+#   exit 1
+# else
+#   cp $HOME/arweave.json $HOME/kyve/integrations/node/
+# fi
 
-tee <<EOF >/dev/null $HOME/kyve/integrations/node/config.json
-{
-  "pools": {
-    "0xbBBfbE9A731634eDdf84C67A106CEE1F981F3f7e": 10
-  }
-}
-EOF
+# tee <<EOF >/dev/null $HOME/kyve/integrations/node/config.json
+# {
+#   "pools": {
+#     "0xbBBfbE9A731634eDdf84C67A106CEE1F981F3f7e": 10
+#   }
+# }
+# EOF
 
-tee <<EOF >/dev/null $HOME/kyve/integrations/node/.env
-CONFIG=config.json
-WALLET=arweave.json
-SEND_STATISTICS=true
-PK=`cat $HOME/metamask.txt`
-EOF
+# tee <<EOF >/dev/null $HOME/kyve/integrations/node/.env
+# CONFIG=config.json
+# WALLET=arweave.json
+# SEND_STATISTICS=true
+# PK=`cat $HOME/metamask.txt`
+# EOF
+#
+# echo "Репозиторий склонирован, конфиг на месте, начинаем билд приложения"
+# echo "-----------------------------------------------------------------------------"
 
-echo "Репозиторий склонирован, конфиг на месте, начинаем билд приложения"
-echo "-----------------------------------------------------------------------------"
-
-cd $HOME/kyve
-yarn setup &>/dev/null
-
-cd $HOME/kyve/integrations/node
-yarn node:build &>/dev/null
-
-docker rm -f kyve &>/dev/null
-docker run -d -it --restart=always --name=kyve kyve-node:latest &>/dev/null
+# cd $HOME/kyve
+# yarn setup &>/dev/null
+#
+# cd $HOME/kyve/integrations/node
+# yarn node:build &>/dev/null
+#
+# docker rm -f kyve &>/dev/null
+# docker run -d -it --restart=always --name=kyve kyve-node:latest &>/dev/null
+docker pull kyve/evm:latest
+docker stop kyve &>/dev/null
+docker container rm kyve &>/dev/null
+docker run -d -it --restart=always \
+--name kyve kyve/evm:latest \
+--pool 0x753924e3f7bdbC877D1D81dD82A61c29a165814E \
+--private-key `cat $HOME/metamask.txt` \
+--stake 101 &>/dev/null
 
 echo "Нода запущена, переходим к следующему пункту гайда"
 echo "-----------------------------------------------------------------------------"
