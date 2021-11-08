2c91c5e7e01bb49303649311a8ed0712a1fa1c94
razumv <razumv95@gmail.com>
2021-11-08 12:13:16 +0200

    fix

--- forta/agent.sh
+++ forta/agent.sh
@@ -6,17 +6,17 @@ echo "Устанавливаем софт"
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_node14.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash &>/dev/null
-sudo apt install python3-pip -y
-npm install -g forta-agent
-pip install virtualenv
+sudo apt install python3-pip -y bash &>/dev/null
+npm install -g forta-agent bash &>/dev/null
+pip install virtualenv bash &>/dev/null
 
-useradd -m user -s /bin/bash
+useradd -m user -s /bin/bash bash &>/dev/null
 sudo su user
 
-virtualenv $HOME/forta
-source $HOME/forta/bin/activate
+virtualenv $HOME/forta bash &>/dev/null
+source $HOME/forta/bin/activate bash &>/dev/null
 cd $HOME/forta
 mkdir my-agent
 cd my-agent
-npm install -g forta-agent
+npm install -g forta-agent bash &>/dev/null
 #npx forta-agent@latest init --typescript
