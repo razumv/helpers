f5d9b1f0425b73cff547030b91c07556f422d47b
razumv <razumv95@gmail.com>
2021-11-15 12:50:41 +0200

    update script auto buy rolls

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -7,23 +7,23 @@ if [ ! -e $HOME/massa/massa-client/massa-client ]; then
 fi
 
 cd $HOME/massa/massa-client
-massa_wallet_address=$(./massa-client --cli true wallet_info | jq -r '.balances | keys[0]')
+massa_wallet_address=$(./massa-client wallet_info | grep Address | awk '{ print $2 }')
 while true
 do
-	balance=$(./massa-client --cli true wallet_info | jq -r '.balances[].final_ledger_data.balance')
-	int_balance=${balance%%.*}
-	if [ $int_balance -gt "99" ]; then
-		echo "More than 99"
-		resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
-		echo $resp
-	elif [ $int_balance -lt "100" ]; then
-		echo "Less than 100"
-	fi
-	printf "sleep"
-	for((sec=0; sec<60; sec++))
-	do
-		printf "."
-		sleep 1
-	done
-	printf "\n"
+        balance=$(./massa-client wallet_info | grep "Final balance" | awk '{ print $3 }')
+        int_balance=${balance%%.*}
+        if [ $int_balance -gt "99" ]; then
+                echo "More than 99"
+                resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                echo $resp
+        elif [ $int_balance -lt "100" ]; then
+                echo "Less than 100"
+        fi
+        printf "sleep"
+        for((sec=0; sec<60; sec++))
+        do
+                printf "."
+                sleep 1
+        done
+        printf "\n"
 done
