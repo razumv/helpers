5a216ad691ee410efc1f142c67aa47da8fee4f74
razumv <razumv95@gmail.com>
2021-11-19 14:42:33 +0200

    fix

--- assetNode/update_peers.sh
+++ assetNode/update_peers.sh
@@ -2,5 +2,5 @@
 sudo systemctl stop assetnode
 assetNode unsafe-reset-all
 persistent_peers=`wget -qO - https://raw.githubusercontent.com/razumv/helpers/main/assetNode/peers.txt | tr '\n' ',' | sed 's%,$%%'`
-sed -i -e "s/^persistent_peers *=.*/persistent_peers = $persistent_peers/" $HOME/.assetNode/config/config.toml
+sed -i -e "s/^persistent_peers *=.*/persistent_peers = \"$persistent_peers\"/" $HOME/.assetNode/config/config.toml
 sudo systemctl start assetnode
