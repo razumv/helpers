f13e7a424aaa4a0c65c35b1b7f86622eb040feec
razumv <razumv95@gmail.com>
2021-11-26 11:14:06 +0200

    update script peers

--- evmos/update_peers.sh
+++ evmos/update_peers.sh
@@ -2,15 +2,7 @@
 sudo systemctl stop evmos
 evmosd unsafe-reset-all
 wget -qO $HOME/.evmosd/config/genesis.json https://github.com/tharsis/testnets/blob/2267211602bb6e004a10a7b6e0395eed7a74b689/olympus_mons/genesis.json &>/dev/null
-bootstrap_node="http://5.189.156.65:26657"; \
-latest_height=`wget -qO- "${bootstrap_node}/block" | jq -r ".result.block.header.height"`; \
-block_height=$((latest_height - 2000)); \
-trust_hash=`wget -qO- "${bootstrap_node}/block?height=${block_height}" | jq -r ".result.block_id.hash"`; \
-sed -i -e "s%^moniker *=.*%moniker = \"$EVMOS_NODENAME\"%; "\
-"s%^seeds *=.*%seeds = \"`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/2267211602bb6e004a10a7b6e0395eed7a74b689/olympus_mons/seeds.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
-"s%^persistent_peers *=.*%persistent_peers = \"`wget -qO - https://raw.githubusercontent.com/razumv/helpers/main/evmos/peers.txt | tr '\n' ',' | sed 's%,$%%'`,`wget -qO - https://raw.githubusercontent.com/tharsis/testnets/2267211602bb6e004a10a7b6e0395eed7a74b689/olympus_mons/peers.txt | tr '\n' ',' | sed 's%,$%%'`\"%; "\
-"s%^enable *=.*%enable = false%; "\
-"s%^rpc_servers *=.*%rpc_servers = \"${bootstrap_node},${bootstrap_node}\"%; "\
-"s%^trust_height *=.*%trust_height = $block_height%; "\
-"s%^trust_hash *=.*%trust_hash = \"$trust_hash\"%" $HOME/.evmosd/config/config.toml
+curl https://raw.githubusercontent.com/tharsis/testnets/main/olympus_mons/peers.txt > peers.txt
+PEERS=`awk '{print $1}' peers.txt | paste -s -d, -`
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" ~/.evmosd/config/config.toml
 sudo systemctl restart evmos
