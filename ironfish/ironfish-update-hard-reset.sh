938c233a697853847cc37e7b9284d17be032b955
razumv <razumv95@gmail.com>
2021-12-01 13:04:19 +0200

    add ironfish update

---
+++ ironfish/ironfish-update-hard-reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wallet_name=`docker exec ironfish ./bin/run accounts:which`
+docker exec ironfish ./bin/run accounts:export $wallet_name wallet
+docker cp ironfish:/usr/src/app/wallet .
+docker-compose down
+docker-compose pull
+docker-compose up -d
+docker exec ironfish-miner ./bin/run reset --confirm
+docker-compose restart
+docker cp wallet ironfish:/usr/src/app/wallet
+docker exec ironfish ./bin/run accounts:import wallet
+docker exec ironfish ./bin/run accounts:use $wallet_name
