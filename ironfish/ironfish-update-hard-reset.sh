767a80266168c8643e3760d7af154ddc8b185263
razumv <razumv95@gmail.com>
2021-12-01 13:07:57 +0200

    fix update

--- ironfish/ironfish-update-hard-reset.sh
+++ ironfish/ironfish-update-hard-reset.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
 cd $HOME
-wallet_name=`docker exec ironfish ./bin/run accounts:which`
-docker exec ironfish ./bin/run accounts:export $wallet_name wallet
+wallet_name=`docker exec ironfish ./bin/run accounts:which` &>/dev/null
+docker exec ironfish ./bin/run accounts:export $wallet_name wallet &>/dev/null
 docker cp ironfish:/usr/src/app/wallet .
-docker-compose down
-docker-compose pull
-docker-compose up -d
-docker exec ironfish-miner ./bin/run reset --confirm
+docker-compose down &>/dev/null
+docker-compose pull &>/dev/null
+docker-compose up -d &>/dev/null
+docker exec ironfish-miner ./bin/run reset --confirm &>/dev/null
 docker-compose restart
 docker cp wallet ironfish:/usr/src/app/wallet
 docker exec ironfish ./bin/run accounts:import wallet
-docker exec ironfish ./bin/run accounts:use $wallet_name
+docker exec ironfish ./bin/run accounts:use $wallet_name &>/dev/null
+echo "-----------------------------------------------------------------------------"
+echo "Обновление завершено"
+echo "-----------------------------------------------------------------------------"
