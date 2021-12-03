927c8a83bf32024bb55f769422ef9e7082003857
razumv <razumv95@gmail.com>
2021-12-03 21:27:39 +0200

    fix

--- aleo/update_miner.sh
+++ aleo/update_miner.sh
@@ -20,18 +20,31 @@ sudo systemctl restart systemd-journald
 
 echo 'export MINER_ADDRESS='$(cat $HOME/account_aleo.txt | awk '/Address/ {print $2}') >> $HOME/.profile
 source $HOME/.profile
-sed -i "s/MINER_ADDRESS=\".*\"/MINER_ADDRESS=\"${MINER_ADDRESS}\"/g" $HOME/snarkOS/run-miner.sh
+# sed -i "s/MINER_ADDRESS=\".*\"/MINER_ADDRESS=\"${MINER_ADDRESS}\"/g" $HOME/snarkOS/run-miner.sh
 
+# sudo tee <<EOF >/dev/null /etc/systemd/system/miner.service
+# [Unit]
+# Description=Aleo miner
+# After=network-online.target
+# [Service]
+# Environment=PATH="/root/.cargo/bin/:$PATH"
+# Environment=MINER_ADDRESS=$(cat $HOME/account_aleo.txt | awk '/Address/ {print $2}')
+# User=$USER
+# WorkingDirectory=$HOME/snarkOS
+# ExecStart=$HOME/snarkOS/run-miner.sh
+# Restart=always
+# RestartSec=10
+# LimitNOFILE=10000
+# [Install]
+# WantedBy=multi-user.target
+# EOF
 sudo tee <<EOF >/dev/null /etc/systemd/system/miner.service
 [Unit]
-Description=Aleo miner
+Description=Aleo Node
 After=network-online.target
 [Service]
-Environment=PATH="/root/.cargo/bin/:$PATH"
-Environment=MINER_ADDRESS=$(cat $HOME/account_aleo.txt | awk '/Address/ {print $2}')
 User=$USER
-WorkingDirectory=$HOME/snarkOS
-ExecStart=$HOME/snarkOS/run-miner.sh
+ExecStart=$HOME/snarkOS/target/release/snarkos --trial --miner $MINER_ADDRESS
 Restart=always
 RestartSec=10
 LimitNOFILE=10000
@@ -42,3 +55,37 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl enable miner
 sudo systemctl restart miner
+
+sudo tee <<EOF >/dev/null $HOME/miner_update.sh
+cd $HOME/snarkOS
+while :
+do
+  echo "Checking for updates..."
+  STATUS=$(git pull)
+
+  echo $STATUS
+
+  if [ "$STATUS" != "Already up to date." ]; then
+	source $HOME/.cargo/env
+	cargo clean
+	cargo build --release
+	# cargo clean
+	if [[ `service miner status | grep active` =~ "running" ]]; then
+	  echo "Aleo Miner is active"
+	  systemctl stop miner
+	  ALEO_IS_MINER=true
+	fi
+	if [[ `echo $ALEO_IS_MINER` =~ "true" ]]; then
+	  echo "Aleo Miner restarted"
+	  systemctl restart miner
+	fi
+  fi
+done
+EOF
+#thanks nodes.guru for this script :)
+
+chmod +x $HOME/miner_update.sh
+
+sudo tee <<EOF >/dev/null /etc/cron.d/
+*/30 * * * * $HOME/miner_update.sh
+EOF
