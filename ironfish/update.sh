c00ddb3189f4cba0b76d3979d4249e81b56638fb
razumv <razumv95@gmail.com>
2021-12-08 14:07:36 +0200

    fix

--- ironfish/update.sh
+++ ironfish/update.sh
@@ -3,33 +3,33 @@ echo "--------------------------------------------------------------------------
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
 echo "-----------------------------------------------------------------------------"
 docker-compose down
-sudo tee <<EOF >/dev/null $HOME/docker-compose.yaml
-version: "3.3"
-services:
- ironfish:
-  container_name: ironfish
-  image: ghcr.io/iron-fish/ironfish:latest
-  restart: always
-  network_mode: "host"
-  entrypoint: sh -c "apt update > /dev/null && apt install curl -y > /dev/null; ./bin/run start"
-  healthcheck:
-   test: "curl -s -H 'Connection: Upgrade' -H 'Upgrade: websocket' http://127.0.0.1:9033 || killall5 -9"
-   interval: 180s
-   timeout: 180s
-   retries: 3
-  volumes:
-   - $HOME/.ironfish:/root/.ironfish
- ironfish-miner:
-  depends_on:
-   - ironfish
-  container_name: ironfish-miner
-  image: ghcr.io/iron-fish/ironfish:latest
-  command: miners:start --threads=-1
-  network_mode: "host"
-  restart: always
-  volumes:
-   - $HOME/.ironfish:/root/.ironfish
-EOF
+# sudo tee <<EOF >/dev/null $HOME/docker-compose.yaml
+# version: "3.3"
+# services:
+#  ironfish:
+#   container_name: ironfish
+#   image: ghcr.io/iron-fish/ironfish:latest
+#   restart: always
+#   network_mode: "host"
+#   entrypoint: sh -c "apt update > /dev/null && apt install curl -y > /dev/null; ./bin/run start"
+#   healthcheck:
+#    test: "curl -s -H 'Connection: Upgrade' -H 'Upgrade: websocket' http://127.0.0.1:9033 || killall5 -9"
+#    interval: 180s
+#    timeout: 180s
+#    retries: 3
+#   volumes:
+#    - $HOME/.ironfish:/root/.ironfish
+#  ironfish-miner:
+#   depends_on:
+#    - ironfish
+#   container_name: ironfish-miner
+#   image: ghcr.io/iron-fish/ironfish:latest
+#   command: miners:start --threads=-1
+#   network_mode: "host"
+#   restart: always
+#   volumes:
+#    - $HOME/.ironfish:/root/.ironfish
+# EOF
 docker-compose pull
 docker-compose up -d
 echo "-----------------------------------------------------------------------------"
