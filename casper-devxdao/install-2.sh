4e7c005b1b69870241f2fa9bcf77d8cb1947d3f2
razumv <razumv95@gmail.com>
2021-12-09 11:21:13 +0200

    add update 1.4.3

--- casper-devxdao/install-2.sh
+++ casper-devxdao/install-2.sh
@@ -52,6 +52,12 @@ sleep 1
 sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_2
 sleep 1
 sudo -u casper /etc/casper/config_from_example.sh 1_4_2
+sleep 1
+sudo -u casper /etc/casper/pull_casper_node_version.sh casper-test.conf 1_4_3
+sleep 1
+sudo -u casper /etc/casper/config_from_example.sh 1_4_3
+sleep 1
+
 
 sudo logrotate -f /etc/logrotate.d/casper-node
 sudo systemctl start casper-node-launcher; sleep 2
