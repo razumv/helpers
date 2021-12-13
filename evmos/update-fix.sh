28b8017162a4aba5a1fae7dfc4341f3c31ffc586
razumv <razumv95@gmail.com>
2021-12-13 13:56:48 +0200

    fix

--- evmos/update-fix.sh
+++ evmos/update-fix.sh
@@ -13,14 +13,24 @@ make install
 
 sed -i.bak -e  "s/^halt-height *=.*/halt-height = 0/" $HOME/.evmosd/config/app.toml
 
-#cosmovisor
-
 cp $HOME/go/bin/evmosd $HOME/.evmosd/cosmovisor/genesis/bin
 mkdir -p $HOME/.evmosd/cosmovisor/upgrades/Olympus-Mons-v0.4.1/bin/
 cp $HOME/go/bin/evmosd $HOME/.evmosd/cosmovisor/upgrades/Olympus-Mons-v0.4.1/bin/
 
-peers="c893e2bf76b60099eecc20d4a9671ed9d4114464@65.21.193.112:26656,867f01d95299fff780c5a5139c2032bc6d773806@167.86.86.48:26656,72160278cbce6192d376816fd715705eb41bc56a@194.163.187.219:26656,bab8164b10a524b608330e787269a349f61a9dd4@135.181.58.43:26656,29558c38f6894066ebafa9f156f2839db9d454f6@23.88.0.168:26656"
+peers="29558c38f6894066ebafa9f156f2839db9d454f6@23.88.0.168:26656"
 
 sed -i.bak -e  "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.evmosd/config/config.toml
+
+SNAP_RPC="http://23.88.0.168:26657"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" ~/.evmosd/config/config.toml
 
 sudo systemctl restart evmos
