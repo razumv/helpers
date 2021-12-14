1373024382857e666366b601020806194bb16d8b
razumv <razumv95@gmail.com>
2021-12-14 14:12:27 +0200

    add peers

--- evmos/update-fix.sh
+++ evmos/update-fix.sh
@@ -17,7 +17,7 @@ cp $HOME/go/bin/evmosd $HOME/.evmosd/cosmovisor/genesis/bin
 mkdir -p $HOME/.evmosd/cosmovisor/upgrades/Olympus-Mons-v0.4.1/bin/
 cp $HOME/go/bin/evmosd $HOME/.evmosd/cosmovisor/upgrades/Olympus-Mons-v0.4.1/bin/
 
-peers="cccdaee68c9f051bd227371424b4d5db6558cbff@144.91.79.203:26656,29558c38f6894066ebafa9f156f2839db9d454f6@23.88.0.168:26656,c893e2bf76b60099eecc20d4a9671ed9d4114464@65.21.193.112:26656"
+peers="99899432a8cdf5b5ff0a9bc6b07dfa240ea3ea33@213.21.221.202:26656,184866519dfa093076ff7a31eddb5bf7b8e1809f@213.21.221.203:26656,a9a1b9845afe75d314b8b5fa762bfed012b3af34@65.21.227.224:26656,cccdaee68c9f051bd227371424b4d5db6558cbff@144.91.79.203:26656,29558c38f6894066ebafa9f156f2839db9d454f6@23.88.0.168:26656,c893e2bf76b60099eecc20d4a9671ed9d4114464@65.21.193.112:26656"
 
 sed -i.bak -e  "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" ~/.evmosd/config/config.toml
 
