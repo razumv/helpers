4edb4d5fd26f05b1e102b8bca4854d07cf551618
razumv <razumv95@gmail.com>
2021-12-17 12:30:43 +0200

    new update

--- zeitgeist/update.sh
+++ zeitgeist/update.sh
@@ -6,8 +6,6 @@ curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.
 
 sudo systemctl stop zeitgeist
 
-# rm -rf $HOME/.local/share/zeitgeist/chains/battery_park/db/
-rm -rf .local/share/zeitgeist/chains/battery_station_mainnet/db/
 rm -f $HOME/zeitgeist/target/release/zeitgeist
 
 wget https://github.com/zeitgeistpm/zeitgeist/releases/download/v0.2.1/zeitgeist_parachain -O $HOME/zeitgeist/target/release/zeitgeist
@@ -18,6 +16,9 @@ if [ ! -e $HOME/zeitgeist_bk/secret_ed25519 ]; then
   mkdir -p $HOME/zeitgeist_bk/
   cp $HOME/.local/share/zeitgeist/rococo/chains/rococo_battery_station_relay_testnet/network/secret_ed25519 $HOME/zeitgeist_bk/
 fi
+
+rm -rf $HOME/.local/share/zeitgeist/chains/battery_station_mainnet/db
+rm -rf $HOME/.local/share/zeitgeist/rococo/chains/rococo_battery_station_relay_testnet/db
 
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
@@ -32,7 +33,8 @@ After=network-online.target
 User=$USER
 Nice=0
 ExecStart=$HOME/zeitgeist/target/release/zeitgeist \
-    --bootnodes=/ip4/45.33.117.205/tcp/30001/p2p/12D3KooWBMSGsvMa2A7A9PA2CptRFg9UFaWmNgcaXRxr1pE1jbe9 \
+--bootnodes=/dns/bsr.zeitgeist.pm/tcp/30337/p2p/12D3KooWSBj4SXAz1ETTurW5PRAF1abyxXb9APAVs4vqBVr2NjRt \
+--bootnodes=/dns/bsr.zeitgeist.pm/tcp/30337/ws/p2p/12D3KooWSBj4SXAz1ETTurW5PRAF1abyxXb9APAVs4vqBVr2NjRt \
     --chain=battery_station \
     --name="$NODENAME | DOUBLETOP" \
     --parachain-id=2050 \
@@ -43,8 +45,8 @@ ExecStart=$HOME/zeitgeist/target/release/zeitgeist \
     --ws-external \
     --rpc-cors=all \
     -- \
-    --bootnodes=/ip4/45.33.117.205/tcp/31001/p2p/12D3KooWHgbvdWFwNQiUPbqncwPmGCHKE8gUQLbzbCzaVbkJ1crJ \
-    --bootnodes=/ip4/45.33.117.205/tcp/31002/p2p/12D3KooWE5KxMrfJLWCpaJmAPLWDm9rS612VcZg2JP6AYgxrGuuE \
+    --bootnodes=/dns/bsr.zeitgeist.pm/tcp/30338/p2p/12D3KooWRuwKV1yt6fqPL6hsfPvJQ28pqmmTxNSDxEC4eLKagzjK \
+    --bootnodes=/dns/bsr.zeitgeist.pm/tcp/30338/ws/p2p/12D3KooWRuwKV1yt6fqPL6hsfPvJQ28pqmmTxNSDxEC4eLKagzjK \
     --chain=$HOME/battery-station-relay.json \
     --port=30334 \
     --rpc-port=9934 \
