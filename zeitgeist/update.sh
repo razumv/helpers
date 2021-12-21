cf77055e097eef989fdaefd34befad283daaafb5
razumv <razumv95@gmail.com>
2021-12-21 22:01:11 +0200

    fix

--- zeitgeist/update.sh
+++ zeitgeist/update.sh
@@ -10,6 +10,7 @@ rm -f $HOME/zeitgeist/target/release/zeitgeist
 
 wget https://github.com/zeitgeistpm/zeitgeist/releases/download/v0.2.1/zeitgeist_parachain -O $HOME/zeitgeist/target/release/zeitgeist
 curl -o $HOME/battery-station-relay.json https://raw.githubusercontent.com/zeitgeistpm/polkadot/battery-station-relay/node/service/res/battery-station-relay.json
+curl -o $HOME/bs_parachain.json https://raw.githubusercontent.com/zeitgeistpm/zeitgeist/f43d0bb1a84cc157fc27b4388e0838db9020dd41/node/res/bs_parachain.json
 chmod +x $HOME/zeitgeist/target/release/zeitgeist
 # mkdir -p $HOME/.local/share/zeitgeist/chains/battery_station_mainnet/
 if [ ! -e $HOME/zeitgeist_bk/secret_ed25519 ]; then
@@ -17,6 +18,7 @@ if [ ! -e $HOME/zeitgeist_bk/secret_ed25519 ]; then
   cp $HOME/.local/share/zeitgeist/rococo/chains/rococo_battery_station_relay_testnet/network/secret_ed25519 $HOME/zeitgeist_bk/
 fi
 
+rm -rf $HOME/.local/share/zeitgeist/chains/zeitgeist/db
 rm -rf $HOME/.local/share/zeitgeist/chains/battery_station_mainnet/db
 rm -rf $HOME/.local/share/zeitgeist/rococo/chains/rococo_battery_station_relay_testnet/db
 
@@ -35,6 +37,7 @@ Nice=0
 ExecStart=$HOME/zeitgeist/target/release/zeitgeist \
     --bootnodes=/dns/bsr.zeitgeist.pm/tcp/30337/p2p/12D3KooWSBj4SXAz1ETTurW5PRAF1abyxXb9APAVs4vqBVr2NjRt \
     --bootnodes=/dns/bsr.zeitgeist.pm/tcp/30337/ws/p2p/12D3KooWSBj4SXAz1ETTurW5PRAF1abyxXb9APAVs4vqBVr2NjRt \
+    --chain=$HOME/bs_parachain.json \
     --name="$NODENAME | DOUBLETOP" \
     --parachain-id=2050 \
     --port=30333 \
