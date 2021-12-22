4e4a613d99fdc2706816ab03db89d2bd438196ed
razumv <razumv95@gmail.com>
2021-12-22 22:25:58 +0200

    fix

--- ironfish/update.sh
+++ ironfish/update.sh
@@ -26,14 +26,13 @@ else
   docker cp ironfish:/usr/src/app/wallet .
   docker-compose down
   docker-compose pull
-  rm -rf $HOME/.ironfish
-  docker-compose up -d &>/dev/null
-  docker cp wallet ironfish:/usr/src/app/wallet
-  docker exec ironfish ./bin/run accounts:import wallet
-  docker exec ironfish ./bin/run accounts:use $wallet_name &>/dev/null
-  docker exec ironfish ./bin/run config:set nodeName $nodeName
-  docker exec ironfish ./bin/run config:set blockGraffiti $blockGraffiti
-  docker-compose restart
+  rm -f $HOME/.ironfish/accounts.backup.json
+  docker-compose run --rm --entrypoint "./bin/run reset --confirm" ironfish
+  docker-compose run -v $HOME/wallet:/usr/src/app/wallet --rm --entrypoint "./bin/run accounts:import wallet" ironfish
+  docker-compose run --rm --entrypoint "./bin/run accounts:use $wallet_name" ironfish &>/dev/null
+  docker-compose run --rm --entrypoint "./bin/run config:set nodeName $nodeName" ironfish
+  docker-compose run --rm --entrypoint "./bin/run config:set blockGraffiti $blockGraffiti" ironfish
+  docker-compose up -d
 fi
 echo "Обновление завершено"
 echo "-----------------------------------------------------------------------------"
