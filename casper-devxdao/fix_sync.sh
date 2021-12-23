5fad723dcb8bb4dd00a72e5accbe64b98e53c36c
razumv <razumv95@gmail.com>
2021-12-23 17:09:09 +0200

    fix

--- casper-devxdao/fix_sync.sh
+++ casper-devxdao/fix_sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo systemctl stop casper-node-launcher
-CASPER_VERSION=1_4_2
+CASPER_VERSION=1_4_3
 # KNOWN_ADDRESSES=$(sudo -u casper cat /etc/casper/$CASPER_VERSION/config.toml | grep known_addresses)
 # KNOWN_VALIDATOR_IPS=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$KNOWN_ADDRESSES")
 # IFS=' ' read -r KNOWN_VALIDATOR_IP _REST <<< "$KNOWN_VALIDATOR_IPS"
