6606675525a69692c89bed03d7168f5b4630ee19
razumv <razumv95@gmail.com>
2022-01-14 16:57:24 +0200

    add wallet_commands

---
+++ celestia/install_1_wallet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $HOME
+source $HOME/.profile
+celestia-appd keys add $CELESTIA_NODENAME &> $HOME/celestia_account.txt
+CELESTIA_ADDR=$(celestia-appd keys show $CELESTIA_WALLET -a)
+echo 'export CELESTIA_ADDR='${CELESTIA_ADDR} >> $HOME/.profile
+CELESTIA_VALOPER=$(celestia-appd keys show $CELESTIA_WALLET --bech val -a)
+echo 'export CELESTIA_VALOPER='${CELESTIA_VALOPER} >> $HOME/.profile
+source $HOME/.profile
