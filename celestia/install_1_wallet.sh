5ec63125901eb2b21448fd201ae9e4b6b567e55e
razumv <razumv95@gmail.com>
2022-01-14 16:59:50 +0200

    fix

--- celestia/install_1_wallet.sh
+++ celestia/install_1_wallet.sh
@@ -2,8 +2,8 @@
 cd $HOME
 source $HOME/.profile
 celestia-appd keys add $CELESTIA_NODENAME &> $HOME/celestia_account.txt
-CELESTIA_ADDR=$(celestia-appd keys show $CELESTIA_WALLET -a)
+CELESTIA_ADDR=$(celestia-appd keys show $CELESTIA_NODENAME -a)
 echo 'export CELESTIA_ADDR='${CELESTIA_ADDR} >> $HOME/.profile
-CELESTIA_VALOPER=$(celestia-appd keys show $CELESTIA_WALLET --bech val -a)
+CELESTIA_VALOPER=$(celestia-appd keys show $CELESTIA_NODENAME --bech val -a)
 echo 'export CELESTIA_VALOPER='${CELESTIA_VALOPER} >> $HOME/.profile
 source $HOME/.profile
