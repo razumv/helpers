26b80a8bd2c5de748e2ca377bbfaeba29403196b
razumv <razumv95@gmail.com>
2022-01-14 15:18:45 +0200

    add celestia install devnet-2

---
+++ celestia/install_1.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+if [ ! $CELESTIA_NODENAME ]; then
+	read -p "Введите ваше имя ноды(придумайте, без спецсимволов - только буквы и цифры): " CELESTIA_NODENAME
+fi
+sleep 1
+CELESTIA_CHAIN="devnet-2"
+echo 'export CELESTIA_CHAIN='$CELESTIA_CHAIN >> $HOME/.profile
+echo 'export CELESTIA_NODENAME='$CELESTIA_NODENAME >> $HOME/.profile
+echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем софт"
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash &>/dev/null
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_go.sh | bash &>/dev/null
+sudo apt install --fix-broken -y &>/dev/null
+sudo apt install nano mc wget -y &>/dev/null
+source .profile
+source .bashrc
+sleep 1
+echo "Весь необходимый софт установлен"
+echo "-----------------------------------------------------------------------------"
+if [ ! -d $HOME/celestia-app ]; then
+  git clone https://github.com/celestiaorg/celestia-app.git &>/dev/null
+fi
+if [ ! -d $HOME/networks ]; then
+  git clone https://github.com/celestiaorg/networks.git &>/dev/null
+fi
+echo "Репозиторий успешно склонирован, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+cd $HOME/celestia-app/
+make install &>/dev/null
+echo "Билд закончен, переходим к инициализации ноды"
+echo "-----------------------------------------------------------------------------"
+celestia-appd init $CELESTIA_NODENAME --chain-id $CELESTIA_CHAIN
+cp $HOME/networks/devnet-2/genesis.json $HOME/.celestia-app/config/
+SEEDS="74c0c793db07edd9b9ec17b076cea1a02dca511f@46.101.28.34:26656"
+PEERS="34d4bfec8998a8fac6393a14c5ae151cf6a5762f@194.163.191.41:26656"
+sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.celestia-app/config/config.toml
+external_address=$(wget -qO- eth0.me)
+sed -i.bak -e "s/^external_address = \"\"/external_address = \"$external_address:26656\"/" $HOME/.celestia-app/config/config.toml
+sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $HOME/.celestia-app/config/config.toml
+sed -i 's/timeout_commit = "5s"/timeout_commit = "15s"/g' $HOME/.celestia-app/config/config.toml
+sed -i 's/index_all_keys = false/index_all_keys = true/g' $HOME/.celestia-app/config/config.toml
+sed -i '/\[api\]/{:a;n;/enabled/s/false/true/;Ta};/\[api\]/{:a;n;/enable/s/false/true/;Ta;}' $HOME/.celestia-app/config/app.toml
+celestia-appd unsafe-reset-all
+wget -O $HOME/.celestia-app/config/addrbook.json "http://62.171.191.122:8000/celestia/addrbook.json"
+
+celestia-appd config chain-id $CELESTIA_CHAIN
+celestia-appd config keyring-backend test
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-appd.service
+[Unit]
+  Description=celestia-appd Cosmos daemon
+  After=network-online.target
+[Service]
+  User=$USER
+  ExecStart=$(which celestia-appd) start
+  Restart=on-failure
+  RestartSec=3
+  LimitNOFILE=4096
+[Install]
+  WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable celestia-appd
+sudo systemctl daemon-reload
+sudo systemctl restart celestia-appd
---
+++ celestia/install_2.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+echo "-----------------------------------------------------------------------------"
+source $HOME/.profile
+if [ ! -d $HOME/celestia-node ]; then
+  git clone https://github.com/celestiaorg/celestia-node.git &>/dev/null
+fi
+echo "Репозиторий успешно склонирован, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+cd $HOME/celestia-node
+git checkout v0.1.1 &>/dev/null
+make install &>/dev/null
+echo "Билд закончен, переходим к инициализации фулл ноды"
+echo "-----------------------------------------------------------------------------"
+
+TRUSTED_SERVER="localhost:26657"
+TRUSTED_HASH=$(curl -s $TRUSTED_SERVER/status | jq -r .result.sync_info.latest_block_hash)
+echo 'export TRUSTED_SERVER='${TRUSTED_SERVER} >> $HOME/.profile
+echo 'export TRUSTED_HASH='${TRUSTED_HASH} >> $HOME/.profile
+source $HOME/.profile
+
+celestia full init --core.remote tcp://127.0.0.1:26657 --headers.trusted-hash $TRUSTED_HASH  &>/dev/null
+sed -i.bak -e 's/PeerExchange = false/PeerExchange = true/g' $HOME/.celestia-full/config.toml
+
+sudo tee /etc/systemd/system/celestia-full.service > /dev/null <<EOF
+[Unit]
+  Description=celestia-full node
+  After=network-online.target
+[Service]
+  User=$USER
+  ExecStart=$(which celestia) full start
+  Restart=on-failure
+  RestartSec=10
+  LimitNOFILE=4096
+[Install]
+  WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable celestia-full
+sudo systemctl daemon-reload
+sudo systemctl restart celestia-full && sleep 10 && journalctl -u celestia-full -o cat -n 10000 --no-pager | grep -m 1 "*  /ip4/" > $HOME/multiaddress.txt
+
+FULL_NODE_IP=$(cat $HOME/multiaddress.txt | sed -r 's/^.{3}//')
+echo 'export FULL_NODE_IP='${FULL_NODE_IP} >> $HOME/.profile
+source $HOME/.profile
+
+echo "Инициализация фулл ноды закончена, переходим к инициализации лайт клиента"
+echo "-----------------------------------------------------------------------------"
+
+rm -rf $HOME/.celestia-light
+celestia light init --headers.trusted-peer $FULL_NODE_IP --headers.trusted-hash $TRUSTED_HASH &>/dev/null
+sed -i.bak -e "s|BootstrapPeers = \[\]|BootstrapPeers = \[\"$FULL_NODE_IP\"\]|g" $HOME/.celestia-light/config.toml
+sed -i.bak -e "s|MutualPeers = \[\]|MutualPeers = \[\"$FULL_NODE_IP\"\]|g" $HOME/.celestia-light/config.toml
+sed -i.bak -e 's/PeerExchange = false/PeerExchange = true/g' $HOME/.celestia-light/config.toml
+sed -i.bak -e 's/Bootstrapper = false/Bootstrapper = true/g' $HOME/.celestia-light/config.toml
+
+sudo tee /etc/systemd/system/celestia-light.service > /dev/null <<EOF
+[Unit]
+  Description=celestia-light
+  After=network-online.target
+[Service]
+  User=$USER
+  ExecStart=$(which celestia) light start
+  Restart=on-failure
+  RestartSec=10
+  LimitNOFILE=4096
+[Install]
+  WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable celestia-light
+sudo systemctl daemon-reload
+sudo systemctl restart celestia-light
+
+echo "Инициализация лайт клиента закончена, установка завершена"
+echo "-----------------------------------------------------------------------------"
