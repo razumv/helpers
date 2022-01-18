572f8bc240245d1c4a12ffc76512b671455c680b
razumv <razumv95@gmail.com>
2022-01-18 09:35:54 +0200

    new update

--- bitcountry/update.sh
+++ bitcountry/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf $HOME/Bit-Country-Blockchain &>/dev/null
-rm -rf $HOME/.local/share/bitcountry-node/chains/tewai_testnet/db/ &>/dev/null
+#rm -rf $HOME/Bit-Country-Blockchain &>/dev/null
+#rm -rf $HOME/.local/share/bitcountry-node/chains/tewai_testnet/db/ &>/dev/null
 
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
@@ -17,12 +17,16 @@ source $HOME/.cargo/env &>/dev/null
 sleep 1
 echo "Весь необходимый софт установлен"
 echo "-----------------------------------------------------------------------------"
+mkdir -p $HOME/bitcountry_bk/
+cp $HOME/.local/share/metaverse-node/chains/tewai_testnet/network/secret_ed25519 $HOME/bitcountry_bk/
+cp $HOME/.local/share/bitcountry-node/chains/tewai_testnet/network/secret_ed25519 $HOME/bitcountry_bk/
 
 if [ ! -d $HOME/Metaverse-Network/ ]; then
   git clone https://github.com/bit-country/Metaverse-Network.git &>/dev/null
 fi
 cd $HOME/Metaverse-Network
-git checkout tewai-v0.0.1 &>/dev/null
+git fetch
+git checkout release-0.0.3 &>/dev/null
 echo "Репозиторий успешно склонирован, начинаем билд"
 echo "-----------------------------------------------------------------------------"
 
@@ -31,31 +35,7 @@ cargo build --release --features=with-tewai-runtime &>/dev/null
 echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
 
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-sudo systemctl restart systemd-journald
-
-sudo tee <<EOF >/dev/null /etc/systemd/system/bitcountry.service
-[Unit]
-Description=Bitcountry Node
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$HOME/Metaverse-Network/target/release/metaverse-node --chain tewai --bootnodes /ip4/13.239.118.231/tcp/30344/p2p/12D3KooW9rDqyS5S5F6oGHYsmFjSdZdX6HAbTD88rPfxYfoXJdNU --name '$NODENAME' --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0'
-Restart=always
-RestartSec=10
-LimitNOFILE=10000
-[Install]
-WantedBy=multi-user.target
-EOF
-
-echo "Сервисные файлы созданы успешно"
-echo "-----------------------------------------------------------------------------"
-
-sudo systemctl daemon-reload
-sudo systemctl enable bitcountry &>/dev/null
 sudo systemctl restart bitcountry
 
-echo "Нода добавлена в автозагрузку на сервере, запущена"
+echo "Нода обновилена и запущена"
 echo "-----------------------------------------------------------------------------"
