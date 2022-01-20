4b57f003652f2f7459c3174e9f012b3c08ce6771
razumv <razumv95@gmail.com>
2022-01-20 22:53:31 +0200

    fix

---
+++ subquery/ex4/3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/subquery/ex4/3/schema.graphql > $HOME/staking-rewards/schema.graphql
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/subquery/ex4/3/src/mappings/mappingHandlers.ts > $HOME/staking-rewards/src/mappings/mappingHandlers.ts
