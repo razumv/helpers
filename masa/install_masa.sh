62c7b823d936773761829eefe1fb3036fdd397d9
razumv <razumv95@gmail.com>
2022-02-27 22:00:23 +0200

    fix

--- masa/install_masa.sh
+++ masa/install_masa.sh
@@ -29,6 +29,7 @@ fi
 echo "Репозиторий успешно склонирован, начинаем билд"
 echo "-----------------------------------------------------------------------------"
 cd $HOME/masa-node-v1.0/src
+git checkout v1.02
 make all &>/dev/null
 go get github.com/ethereum/go-ethereum/accounts/keystore &>/dev/null
 cd $HOME/masa-node-v1.0/src/build/bin
