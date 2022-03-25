3429ab99fbebbdc6b45a61775f7e9a49b92abbb2
razumv <razumv95@gmail.com>
2022-03-25 12:07:02 +0200

    fix

--- aptos/update.sh
+++ aptos/update.sh
@@ -15,12 +15,13 @@ echo "--------------------------------------------------------------------------
 echo -e "\e[1m\e[32m2. Скачиваем конфиги... \e[0m" && sleep 1
 echo "-----------------------------------------------------------------------------"
 rm -f $HOME/aptos/waypoint.txt $HOME/aptos/genesis.blob
-wget https://devnet.aptoslabs.com/genesis.blob 
+wget https://devnet.aptoslabs.com/genesis.blob
 wget https://devnet.aptoslabs.com/waypoint.txt
 echo "-----------------------------------------------------------------------------"
 echo -e "\e[1m\e[32m3. Очищаем бд... \e[0m" && sleep 1
 echo "-----------------------------------------------------------------------------"
 rm -rf /var/lib/docker/volumes/aptos_db/_data/*
+echo "бд удалена"
 echo "-----------------------------------------------------------------------------"
 echo -e "\e[1m\e[32m4. Запускаем Full-node... \e[0m" && sleep 1
 echo "-----------------------------------------------------------------------------"
