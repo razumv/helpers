851b553bc67ea1cb4903c8b560aae4e0e7db3fa7
razumv <razumv95@gmail.com>
2022-04-01 13:52:06 +0300

    fix update aptos

---
+++ aptos/install.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+colors
+line
+logo
+line
+echo -e "${GREEN}1. Updating list of dependencies... ${NORMAL}" && sleep 1
+line
+install_deps
+
+echo -e "${GREEN}2 Проверяем наличие бинарников aptos-operational-tool aptos-node... ${NORMAL}" && sleep 1
+line
+if ! command -v aptos-operational-tool &> /dev/null
+then
+  source_code
+  build_tools
+else
+  fetch_code
+  build_tools
+fi
+if ! command -v aptos-node &> /dev/null
+then
+  source_code
+  build_node
+else
+  fetch_code
+  build_node
+fi
+line
+echo -e "${GREEN}3. Downloading Aptos FullNode config files ... ${NORMAL}" && sleep 1
+update_genesis_files
+line
+echo -e "${GREEN}4.1 Создание идентити ${NORMAL}"
+create_identity
+get_vars
+if [ ! -z "$PRIVATE_KEY" ]
+then
+    echo -e "\e[1m\e[92m Identity was successfully created ${NORMAL}"
+    echo -e "\e[1m\e[92m Peer Id: ${NORMAL}" $PEER_ID
+    echo -e "\e[1m\e[92m Private Key:  ${NORMAL}" $PRIVATE_KEY
+else
+    rm $HOME/aptos/identity/id.json
+    rm $HOME/aptos/identity/private-key.txt
+    echo -e "\e[1m\e[91m Wasn't able to create the Identity. FullNode will be started without the identity, identity can be added manually ${NORMAL}"
+fi
+
+if [[ -f $HOME/aptos/identity/private-key.txt ]]
+then
+    get_vars
+    if [ ! -z "$PRIVATE_KEY" ]
+    then
+        echo -e "\e[1m\e[92m Peer Id: ${NORMAL}" $PEER_ID
+        echo -e "\e[1m\e[92m Private Key:  ${NORMAL}" $PRIVATE_KEY
+    else
+        rm $HOME/aptos/identity/private-key.txt
+        create_identity
+    fi
+    line
+else
+    create_identity
+    line
+fi
+
+get_vars
+fix_config
+line
+echo -e "${GREEN}5. Запуск Aptos FullNode ... ${NORMAL}" && sleep 5
+line
+fix_journal
+bin_service
+line
+echo -e "${GREEN}Aptos FullNode запущена ${NORMAL}"
+line
+
+if [ ! -z "$PRIVATE_KEY" ]
+then
+    echo -e "${GREEN}Путь к приватнику, рекомендуется забекапить: ${NORMAL}"
+    echo -e "${RED}"    $HOME/aptos/identity/private-key.txt" \n ${NORMAL}"
+    echo -e "${GREEN}Путь к файлу пира, рекомендуется забекапить: ${NORMAL}"
+    echo -e "${RED}"    $HOME/aptos/identity/peer-info.yaml" \n ${NORMAL}"
+fi
+
+echo -e "${GREEN}Для проверки синка: ${NORMAL}"
+echo -e "${RED}    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n ${NORMAL}"
+
+echo -e "${GREEN}Для проверки логов: ${NORMAL}"
+echo -e "${RED}    journalctl -n 100 -f -u aptos \n ${NORMAL}"
+
+echo -e "${GREEN}Для остановки: ${NORMAL}"
+echo -e "${RED}    sudo systemctl stop aptos \n ${NORMAL}"
+
+install_deps {
+  curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_ufw.sh | bash &>/dev/null
+  curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_rust.sh | bash &>/dev/null
+  sudo apt-get install jq mc wget git -y &>/dev/null
+  sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 &>/dev/null
+  sudo chmod a+x /usr/local/bin/yq
+}
+
+source_code {
+  if [ ! -d $HOME/aptos-core ]; then
+    git clone https://github.com/aptos-labs/aptos-core.git
+  fi
+  cd $HOME/aptos-core
+  git fetch
+  git checkout origin/devnet
+  echo y | ./scripts/dev_setup.sh
+  source ~/.cargo/env
+}
+
+fetch_code {
+  cd $HOME/aptos-core
+  git fetch && git pull
+}
+
+update_genesis_files {
+  cd $HOME/aptos/
+  rm -f $HOME/aptos/waypoint.txt $HOME/aptos/genesis.blob
+  wget https://devnet.aptoslabs.com/genesis.blob
+  wget https://devnet.aptoslabs.com/waypoint.txt
+}
+
+build_tools {
+  cargo build -p aptos-operational-tool --release
+  mv $HOME/aptos-core/target/release/aptos-operational-tool /usr/local/bin
+}
+
+build_node {
+  cargo build -p aptos-node --release
+  mv $HOME/aptos-core/target/release/aptos-node /usr/local/bin
+}
+
+create_identity {
+  sudo mkdir -p $HOME/aptos/identity
+  aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file $HOME/aptos/identity/private-key.txt
+  aptos-operational-tool extract-peer-from-file --encoding hex --key-file $HOME/aptos/identity/private-key.txt --output-file $HOME/aptos/identity/peer-info.yaml
+  sleep 1
+}
+
+get_vars {
+  PEER_ID=$(sed -n 2p $HOME/aptos/identity/peer-info.yaml | sed 's/.$//')
+  PRIVATE_KEY=$(cat $HOME/aptos/identity/private-key.txt)
+  WAYPOINT=$(cat $HOME/aptos/waypoint.txt)
+}
+
+fix_config {
+  cp $HOME/aptos-core/config/src/config/test_data/public_full_node.yaml $HOME/aptos/public_full_node.yaml
+  /usr/local/bin/yq e -i '.full_node_networks[] +=  { "identity": {"type": "from_config", "key": "'$PRIVATE_KEY'", "peer_id": "'$PEER_ID'"} }' $HOME/aptos/public_full_node.yaml
+  sed -i 's|127.0.0.1|0.0.0.0|' $HOME/aptos/public_full_node.yaml
+  sed -i -e "s|genesis_file_location: .*|genesis_file_location: \"$HOME\/aptos\/genesis.blob\"|" $HOME/aptos/public_full_node.yaml
+  sed -i -e "s|data_dir: .*|data_dir: \"$HOME\/aptos\/data\"|" $HOME/aptos/public_full_node.yaml
+  sed -i -e "s|0:01234567890ABCDEFFEDCA098765421001234567890ABCDEFFEDCA0987654210|$WAYPOINT|" $HOME/aptos/public_full_node.yaml
+}
+
+fix_journal {
+  sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+  Storage=persistent
+EOF
+  sudo systemctl restart systemd-journald
+}
+
+bin_service {
+  sudo tee <<EOF >/dev/null /etc/systemd/system/aptos.service
+  [Unit]
+    Description=Aptos daemon
+    After=network-online.target
+  [Service]
+    User=$USER
+    ExecStart=/usr/local/bin/aptos-node -f $HOME/aptos/public_full_node.yaml
+    Restart=on-failure
+    RestartSec=3
+    LimitNOFILE=4096
+  [Install]
+    WantedBy=multi-user.target
+EOF
+
+  sudo systemctl enable aptos
+  sudo systemctl daemon-reload
+  sudo systemctl restart aptos
+  echo "Сервис обновлен, демон перезагружен"
+}
+
+logo {
+  curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+}
+
+line {
+  echo "-----------------------------------------------------------------------------"
+}
+
+colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
--- aptos/update.sh
+++ aptos/update.sh
@@ -1,62 +1,141 @@
 #!/bin/bash
-echo "-----------------------------------------------------------------------------"
-curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
-echo "-----------------------------------------------------------------------------"
-echo -e "\e[1m\e[32mНачинаем обновление... \e[0m" && sleep 1
-echo "-----------------------------------------------------------------------------"
-echo -e "\e[1m\e[32m1. Стопаем Aptos... \e[0m" && sleep 1
-echo "-----------------------------------------------------------------------------"
+colors
+line
+logo
+line
+echo -e "${GREEN}Начинаем обновление... ${NORMAL}" && sleep 1
+line
+echo -e "${GREEN}1. Стопаем Aptos... ${NORMAL}" && sleep 1
+line
 sudo systemctl stop aptos
-echo "-----------------------------------------------------------------------------"
-echo -e "\e[1m\e[32m2. Скачиваем конфиги... \e[0m" && sleep 1
-echo "-----------------------------------------------------------------------------"
-rm -f $HOME/aptos/waypoint.txt $HOME/aptos/genesis.blob
-wget https://devnet.aptoslabs.com/genesis.blob
-wget https://devnet.aptoslabs.com/waypoint.txt
-echo "-----------------------------------------------------------------------------"
-echo -e "\e[1m\e[32m3. Обновляем код... \e[0m" && sleep 1
-echo "-----------------------------------------------------------------------------"
+line
+echo -e "${GREEN}2. Скачиваем конфиги... ${NORMAL}" && sleep 1
+line
+update_genesis_files
+line
+echo -e "${GREEN}3. Обновляем код... ${NORMAL}" && sleep 1
+line
 if ! command -v aptos-operational-tool &> /dev/null
 then
-  git clone https://github.com/aptos-labs/aptos-core.git  &> /dev/null
-  cd $HOME/aptos-core  &> /dev/null
-  git fetch &>/dev/null
-  git checkout origin/devnet &>/dev/null
-  echo y | ./scripts/dev_setup.sh  &> /dev/null
-  source ~/.cargo/env
-  cargo build -p aptos-operational-tool --release  &> /dev/null
-  mv $HOME/aptos-core/target/release/aptos-operational-tool /usr/local/bin  &> /dev/null
+  source_code
+  build_tools
 else
-  cd $HOME/aptos-core
-  git fetch && git pull
-  cargo build -p aptos-operational-tool --release  &> /dev/null
-  mv $HOME/aptos-core/target/release/aptos-operational-tool /usr/local/bin  &> /dev/null
+  fetch_code
+  build_tools
 fi
 if ! command -v aptos-node &> /dev/null
 then
-  git clone https://github.com/aptos-labs/aptos-core.git  &> /dev/null
-  cd $HOME/aptos-core  &> /dev/null
-  git fetch &>/dev/null
-  git checkout origin/devnet &>/dev/null
-  echo y | ./scripts/dev_setup.sh  &> /dev/null
-  source ~/.cargo/env
-  cargo build -p aptos-node --release &> /dev/null
-  mv $HOME/aptos-core/target/release/aptos-node /usr/local/bin  &> /dev/null
+  source_code
+  build_node
 else
+  fetch_code
+  build_node
+fi
+line
+echo -e "${GREEN}4. Фиксим конфиг... ${NORMAL}" && sleep 1
+line
+get_vars
+fix_config
+delete_old_database
+line
+echo -e "${GREEN}5. Запускаем Full-node... ${NORMAL}" && sleep 1
+line
+fix_journal
+bin_service
+line
+echo -e "${GREEN}Обновление завершено... ${NORMAL}" && sleep 1
+line
+
+source_code {
+  if [ ! -d $HOME/aptos-core ]; then
+    git clone https://github.com/aptos-labs/aptos-core.git
+  fi
+  cd $HOME/aptos-core
+  git fetch
+  git checkout origin/devnet
+  echo y | ./scripts/dev_setup.sh
+  source ~/.cargo/env
+}
+
+fetch_code {
   cd $HOME/aptos-core
   git fetch && git pull
-  cargo build -p aptos-node --release &> /dev/null
-  mv $HOME/aptos-core/target/release/aptos-node /usr/local/bin  &> /dev/null
-fi
-# echo "-----------------------------------------------------------------------------"
-# echo -e "\e[1m\e[32m3. Очищаем бд... \e[0m" && sleep 1
-# echo "-----------------------------------------------------------------------------"
-# rm -rf $HOME/aptos/data
-# echo "бд удалена"
-echo "-----------------------------------------------------------------------------"
-echo -e "\e[1m\e[32m4. Запускаем Full-node... \e[0m" && sleep 1
-echo "-----------------------------------------------------------------------------"
-sudo systemctl start aptos
-echo "-----------------------------------------------------------------------------"
-echo -e "\e[1m\e[32mОбновление завершено... \e[0m" && sleep 1
-echo "-----------------------------------------------------------------------------"
+}
+
+update_genesis_files {
+  cd $HOME/aptos/
+  rm -f $HOME/aptos/waypoint.txt $HOME/aptos/genesis.blob
+  wget https://devnet.aptoslabs.com/genesis.blob
+  wget https://devnet.aptoslabs.com/waypoint.txt
+}
+
+build_tools {
+  cargo build -p aptos-operational-tool --release
+  mv $HOME/aptos-core/target/release/aptos-operational-tool /usr/local/bin
+}
+
+build_node {
+  cargo build -p aptos-node --release
+  mv $HOME/aptos-core/target/release/aptos-node /usr/local/bin
+}
+
+get_vars {
+  PEER_ID=$(sed -n 2p $HOME/aptos/identity/peer-info.yaml | sed 's/.$//')
+  PRIVATE_KEY=$(cat $HOME/aptos/identity/private-key.txt)
+  WAYPOINT=$(cat $HOME/aptos/waypoint.txt)
+}
+
+fix_config {
+  cp $HOME/aptos-core/config/src/config/test_data/public_full_node.yaml $HOME/aptos/public_full_node.yaml
+  /usr/local/bin/yq e -i '.full_node_networks[] +=  { "identity": {"type": "from_config", "key": "'$PRIVATE_KEY'", "peer_id": "'$PEER_ID'"} }' $HOME/aptos/public_full_node.yaml
+  sed -i 's|127.0.0.1|0.0.0.0|' $HOME/aptos/public_full_node.yaml
+  sed -i -e "s|genesis_file_location: .*|genesis_file_location: \"$HOME\/aptos\/genesis.blob\"|" $HOME/aptos/public_full_node.yaml
+  sed -i -e "s|data_dir: .*|data_dir: \"$HOME\/aptos\/data\"|" $HOME/aptos/public_full_node.yaml
+  sed -i -e "s|0:01234567890ABCDEFFEDCA098765421001234567890ABCDEFFEDCA0987654210|$WAYPOINT|" $HOME/aptos/public_full_node.yaml
+}
+
+delete_old_database {
+  rm -rf $HOME/aptos/data/
+}
+
+fix_journal {
+  sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+  Storage=persistent
+EOF
+  sudo systemctl restart systemd-journald
+}
+
+bin_service {
+  sudo tee <<EOF >/dev/null /etc/systemd/system/aptos.service
+  [Unit]
+    Description=Aptos daemon
+    After=network-online.target
+  [Service]
+    User=$USER
+    ExecStart=/usr/local/bin/aptos-node -f $HOME/aptos/public_full_node.yaml
+    Restart=on-failure
+    RestartSec=3
+    LimitNOFILE=4096
+  [Install]
+    WantedBy=multi-user.target
+EOF
+
+  sudo systemctl enable aptos
+  sudo systemctl daemon-reload
+  sudo systemctl restart aptos
+  echo "Сервис обновлен, демон перезагружен"
+}
+
+logo {
+  curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
+}
+
+line {
+  echo "-----------------------------------------------------------------------------"
+}
+
+colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
