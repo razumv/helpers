9d623b494679c80de393deac7bfb1a1a0d88c91e
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-04-09 15:59:18 +0300

    fix

--- celestia/update.sh
+++ celestia/update.sh
@@ -2,11 +2,21 @@
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
 echo "-----------------------------------------------------------------------------"
+cd $HOME
+ver="1.17.2"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+echo "Софт успешно обновлен, начинаем обновление репрозитория"
+echo "-----------------------------------------------------------------------------"
 sudo systemctl stop celestia-appd celestia-full celestia-light
 cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
-echo "Репозиторий успешно склонирован, начинаем билд"
+echo "Репозиторий успешно обновлен, начинаем билд"
 echo "-----------------------------------------------------------------------------"
 cd celestia-node
 git checkout v0.2.0
