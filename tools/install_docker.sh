8e51d8d00a66b8788ecf8e9bff1f542595c94deb
razumv <razumv95@gmail.com>
2022-04-09 13:09:25 +0300

    fix

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -4,6 +4,6 @@ sudo apt install wget -y
 wget -O get-docker.sh https://get.docker.com
 sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 rm -f get-docker.sh
 sudo usermod -aG docker $USER
