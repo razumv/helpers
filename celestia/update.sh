7e42d550e7f9a242539b3b71a20f79e548978137
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-04-09 17:56:22 +0300

    fix

--- celestia/update.sh
+++ celestia/update.sh
@@ -2,28 +2,28 @@
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/doubletop.sh | bash
 echo "-----------------------------------------------------------------------------"
-cd $HOME &>/dev/null
-ver="1.17.2" &>/dev/null
+cd $HOME
+ver="1.17.2"
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" &>/dev/null
-sudo rm -rf /usr/local/go &>/dev/null
+sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" &>/dev/null
-rm "go$ver.linux-amd64.tar.gz" &>/dev/null
+rm "go$ver.linux-amd64.tar.gz"
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile &>/dev/null
-source $HOME/.bash_profile &>/dev/null
+source $HOME/.bash_profile
 echo "Софт успешно обновлен, начинаем обновление репрозитория"
 echo "-----------------------------------------------------------------------------"
-sudo systemctl stop celestia-appd celestia-full celestia-light &>/dev/null
-cd $HOME &>/dev/null
+sudo systemctl stop celestia-appd celestia-full celestia-light
+cd $HOME
 rm -rf celestia-node &>/dev/null
 git clone https://github.com/celestiaorg/celestia-node.git &>/dev/null
 echo "Репозиторий успешно обновлен, начинаем билд"
 echo "-----------------------------------------------------------------------------"
-cd celestia-node &>/dev/null
-git checkout v0.2.0 &>/dev/null
+cd celestia-node
+git checkout v0.2.0
 make install &>/dev/null
 echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
-mv $HOME/.celestia-full $HOME/.celestia-bridge &>/dev/null
+mv $HOME/.celestia-full $HOME/.celestia-bridge
 sudo tee /etc/systemd/system/celestia-bridge.service > /dev/null <<EOF
 [Unit]
   Description=celestia-bridge
@@ -39,8 +39,8 @@ sudo tee /etc/systemd/system/celestia-bridge.service > /dev/null <<EOF
 EOF
 
 sudo systemctl enable celestia-bridge &>/dev/null
-sudo systemctl daemon-reload &>/dev/null
-sudo systemctl restart celestia-bridge celestia-appd celestia-light &>/dev/null
+sudo systemctl daemon-reload
+sudo systemctl restart celestia-bridge celestia-appd celestia-light 
 
 echo "Нода обновилена и запущена"
 echo "-----------------------------------------------------------------------------"
