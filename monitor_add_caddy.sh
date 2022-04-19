8255b4658d6df45f693181bcf386e7741cac3f42
razumv <razumv95@gmail.com>
2022-04-19 13:28:11 +0300

    add caddy config for prom

---
+++ monitor_add_caddy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cat <<EOF | tee -a /etc/prometheus/prometheus.yml
+  - job_name: "caddy"
+    scrape_interval: 30s
+    static_configs:
+      - targets: ["localhost:2019"]
+    relabel_configs:
+      - source_labels: [__address__]
+        regex: '.*'
+        target_label: instance
+        replacement: '$HOSTNAME'
+EOF
+
+sudo systemctl restart vmagent
