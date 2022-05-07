c2df20debb64114500ea1cf49e89056ece540e85
razumv <razumv95@gmail.com>
2022-05-07 16:13:59 +0300

    fix

--- casper-devxdao/fix_sync.sh
+++ casper-devxdao/fix_sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo systemctl stop casper-node-launcher
-TRUSTED_HASH=32c874bc3a383b5a3e2daad19598202abd60170028b540e022a7f32067a2f446
+TRUSTED_HASH=8dcde9e4ee841a617d90e8f29c48a1e3c0cd36e5ef227ceaec0fda329b7d4846
 if [ "$TRUSTED_HASH" != "null" ]; then sudo -u casper sed -i "/trusted_hash =/c\trusted_hash = '$TRUSTED_HASH'" /etc/casper/*/config.toml; fi
 sudo logrotate -f /etc/logrotate.d/casper-node
 sudo systemctl start casper-node-launcher
