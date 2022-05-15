f8046b73d21c35ea72ca89dcb9c4899d4608cc37
razumv <razumv95@gmail.com>
2022-05-15 13:37:41 +0300

    fix

--- aptos/stop_old_aptos.sh
+++ aptos/stop_old_aptos.sh
@@ -2,7 +2,7 @@
 
 sudo systemctl stop aptos  &>/dev/null
 sudo systemctl disable aptos &>/dev/null
-cd $HOME/testnet/
+cd $HOME/testnet/  &>/dev/null
 docker-compose down &>/dev/null
 docker volume rm aptos-fullnode aptos-validator &>/dev/null
 cd $HOME/aptos_testnet/ &>/dev/null
