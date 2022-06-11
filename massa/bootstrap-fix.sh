5da340792e22413726b2421fb766f215f49988e3
razumv <razumv95@gmail.com>
2022-06-11 15:03:18 +0300

    fix

--- massa/bootstrap-fix.sh
+++ massa/bootstrap-fix.sh
@@ -16,19 +16,19 @@ function line {
 
 
 function replace_bootstraps {
-	local config_path="$HOME/massa/massa-node/base_config/config.toml"
-	local bootstrap_list=`wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/bootstrap_list.txt | shuf -n50 | awk '{ print "        "$0"," }'`
-	local len=`wc -l < "$config_path"`
-	local start=`grep -n bootstrap_list "$config_path" | cut -d: -f1`
-	local end=`grep -n "\[optionnal\] port on which to listen" "$config_path" | cut -d: -f1`
-	local end=$((end-1))
-	local first_part=`sed "${start},${len}d" "$config_path"`
-	local second_part="
+	config_path="$HOME/massa/massa-node/base_config/config.toml"
+	bootstrap_list=`wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/bootstrap_list.txt | shuf -n50 | awk '{ print "        "$0"," }'`
+	len=`wc -l < "$config_path"`
+	start=`grep -n bootstrap_list "$config_path" | cut -d: -f1`
+	end=`grep -n "\[optionnal\] port on which to listen" "$config_path" | cut -d: -f1`
+	end=$((end-1))
+	first_part=`sed "${start},${len}d" "$config_path"`
+	second_part="
     bootstrap_list = [
 ${bootstrap_list}
     ]
 "
-	local third_part=`sed "1,${end}d" "$config_path"`
+	third_part=`sed "1,${end}d" "$config_path"`
 	echo "${first_part}${second_part}${third_part}" > "$config_path"
 	sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
   sudo systemctl restart massa
