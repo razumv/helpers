afb26c4133bda4a8158fbaf48e0b63ccdfd39ff2
razumv <razumv95@gmail.com>
2022-06-11 14:55:30 +0300

    fix massa

--- massa/bootstrap-fix.sh
+++ massa/bootstrap-fix.sh
@@ -31,6 +31,7 @@ ${bootstrap_list}
 	local third_part=`sed "1,${end}d" "$config_path"`
 	echo "${first_part}${second_part}${third_part}" > "$config_path"
 	sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
+  sudo systemctl restart massa
 }
 #Thanks to Let's Node!
 
