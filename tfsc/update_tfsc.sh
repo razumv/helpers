4c2e29cd89dd18a64ec50fd062c874a1515f7ef3
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-07-15 12:38:39 +0300

    upd

--- tfsc/update_tfsc.sh
+++ tfsc/update_tfsc.sh
@@ -3,7 +3,7 @@
 pkill -9 tfsc
 
 cd $HOME/tfsc/
-wget -O $HOME/tfsc/tfsc http://fastcdn.uscloudmedia.com/transformers/test/ttfsc_0.0.1_testnet
+wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_0.0.2_8223c46_devnet
 chmod +x $HOME/tfsc/tfsc
 
 tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfsc -m'
