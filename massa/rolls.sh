8903244aa61fd1edf36162ff570aa8ec6a7b4492
KURASH <92112412+kuraassh@users.noreply.github.com>
2022-07-17 13:33:39 +0300

    fix

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -15,7 +15,7 @@ do
         int_balance=${balance%%.*}
         if [ $int_balance -gt "99" ]; then
                 echo "More than 99"
-                resp=$(./massa-client buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                resp=$(./massa-client --pwd $massa_pass buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
                 echo $resp
         elif [ $int_balance -lt "100" ]; then
                 echo "Less than 100"
