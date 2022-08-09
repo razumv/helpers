c6e48c7b74125cbf1064eaeb3daa1f81f1c932b8
razumv <razumv95@gmail.com>
2022-08-09 12:28:52 +0200

    update

--- tfsc/tfsc.sh
+++ tfsc/tfsc.sh
@@ -26,7 +26,7 @@ echo -e "${GREEN}Скачиваем tfsc${NORMAL}"
 line
 mkdir -p $HOME/tfsc
 cd $HOME/tfsc/
-wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.1.1_89ce99f_devnet
+wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.1.4_6a305a5_devnet
 chmod +x $HOME/tfsc/tfsc
 line
 echo -e "${GREEN}Конфигурируем и запускаем tfsc${NORMAL}"
