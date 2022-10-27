bffcf0142ed9ffe0b32d50882cead25bfd9deb1e
Ivan <92527194+nearivan@users.noreply.github.com>
2022-10-27 17:22:20 +0300

    Update rolls.sh
    massa working derictory fix

--- massa/rolls.sh
+++ massa/rolls.sh
@@ -8,14 +8,14 @@
 # fi
 # #
 cd $HOME/massa/massa-client
-massa_wallet_address=$(./massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
+massa_wallet_address=$(./massa/massa-client --pwd $massa_pass wallet_info | grep Address | awk '{ print $2 }')
 while true
 do
-        balance=$(./massa-client --pwd $massa_pass wallet_info | grep "Final balance" | awk '{ print $3 }')
+        balance=$(./massa/massa-client --pwd $massa_pass wallet_info | grep "Final balance" | awk '{ print $3 }')
         int_balance=${balance%%.*}
         if [ $int_balance -gt "99" ]; then
                 echo "More than 99"
-                resp=$(./massa-client --pwd $massa_pass buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
+                resp=$(./massa/massa-client --pwd $massa_pass buy_rolls $massa_wallet_address $(($int_balance/100)) 0)
                 echo $resp
         elif [ $int_balance -lt "100" ]; then
                 echo "Less than 100"
