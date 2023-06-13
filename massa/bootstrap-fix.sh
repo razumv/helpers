45afe84e507014bd981f288ef8d4c9a75c9caa91
razumv <42905535+razumv@users.noreply.github.com>
2023-06-13 11:57:04 +0200

    Update bootstrap-fix.sh

--- massa/bootstrap-fix.sh
+++ massa/bootstrap-fix.sh
@@ -17,7 +17,7 @@ function line {
 
 function replace_bootstraps {
 	config_path="$HOME/massa/massa-node/base_config/config.toml"
-	bootstrap_list=`wget -qO- https://raw.githubusercontent.com/razumv/helpers/main/massa/bootstrap_list.txt | shuf -n50 | awk '{ print "        "$0"," }'`
+	bootstrap_list=`wget -qO- https://raw.githubusercontent.com/razumv/helpers/main/massa/bootstrap_list.txt | shuf -n500 | awk '{ print "        "$0"," }'`
 	len=`wc -l < "$config_path"`
 	start=`grep -n bootstrap_list "$config_path" | cut -d: -f1`
 	end=`grep -n "force the bootstrap protocol to use" "$config_path" | cut -d: -f1`
